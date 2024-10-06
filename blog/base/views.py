683589397da8c45284c8134e8cf99bfcd3950bcd
Davidson <deivisonj1@hotmail.com>
2024-10-06 20:21:08 -0300

    README.md

--- blog/base/views.py
+++ blog/base/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'base/post_list.html', {'posts': posts})
 
-def home(request):
-    return render(request, 'ola mundo')
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)  # Busca o post com o slug correspondente
+    return render(request, 'base/post_detail.html', {'post': post})
+
