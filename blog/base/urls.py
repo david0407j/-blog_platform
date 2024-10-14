f1c1e7370951bd7c9cc6bbca207de2a4be66cc59
Davidson <deivisonj1@hotmail.com>
2024-10-13 21:27:11 -0300

    alteração da lista dos posts

--- blog/base/urls.py
+++ blog/base/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.post_list, name="post_list"),
-    path("post/<slug:slug>/", views.post_detail, name="post_detail"),
+    path("", views.post_list, name="post_list"),  # URL da lista de posts
+    path(
+        "post/<slug:slug>/", views.post_detail, name="post_detail"
+    ),  # URL para detalhes do post
 ]
